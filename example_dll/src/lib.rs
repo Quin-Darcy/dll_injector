e6108e35f3e0809e57b499bdf7544007b95a22fa
Quin Darcy <pohmsuindraguli@gmail.com>
2023-07-08 19:52:10 -0700

    Adding logic for debugging

--- example_dll/src/lib.rs
+++ example_dll/src/lib.rs
@@ -8,7 +8,9 @@ use std::ffi::{OsStr, CStr};
 use std::fmt;
 use std::iter::once;
 use std::ptr::null_mut;
-use winapi::um::winnt::{IMAGE_DOS_HEADER, IMAGE_NT_HEADERS, IMAGE_IMPORT_DESCRIPTOR};
+use winapi::shared::windef::HWND;
+use winapi::shared::minwindef::UINT;
+use winapi::um::winnt::{IMAGE_DOS_HEADER, IMAGE_NT_HEADERS, IMAGE_IMPORT_DESCRIPTOR, LPCSTR};
 
 
 #[derive(Debug)]
@@ -90,9 +92,9 @@ fn begin_hooking(target_module_name: &str, target_function_name: &str) {
         let iat_addr = iat_int_addrs.1;
 
         // Now that we have the address of the INT and IAT, we need the address of the target function
-        let target_func_addr: usize = match get_func_address_in_iat(int_addr, iat_addr, exe_base_addr, target_function_name) {
-            Ok(addr) => {
-                addr
+        let target_func_data: (usize, *mut usize) = match get_func_address_in_iat(int_addr, iat_addr, exe_base_addr, target_function_name) {
+            Ok(data) => {
+                data
             },
             Err(e) => {
                 test_msgbox("Failed to get function address", format!("{}", e).as_str());
@@ -100,10 +102,30 @@ fn begin_hooking(target_module_name: &str, target_function_name: &str) {
             },
         };
 
-        test_msgbox("Target function address: 0x", format!("{:X}", target_func_addr).as_str());
+        let target_func_addr: usize = target_func_data.0;
+        let target_func_addr_ptr: *mut usize = target_func_data.1;
+
+        // Finally, we need the address of the hook function
+        let hook_func_addr: usize = hook_func as usize;
+
+        let breakpoint1: usize = get_func_address_in_iat as usize;
+        let breakpoint2: usize = perform_hook as usize;
+        let breakpoint3: usize = bye as usize;
+
+        test_msgbox("IAT entry", format!("{:X}", target_func_addr_ptr as usize).as_str());
+        test_msgbox("Breakpoint 1", format!("{:X}", breakpoint1).as_str());
+        test_msgbox("Breakpoint 2", format!("{:X}", breakpoint2).as_str());
+        test_msgbox("Breakpoint 3", format!("{:X}", breakpoint3).as_str());
+
+        // Now that we have all the addresses we need, we can perform the hook
+        perform_hook(target_func_addr_ptr, hook_func_addr);
 
         // ----- perform the actual hooking here -----
     }
+}
+
+fn bye() {
+    test_msgbox("Bye", "Bye");
 }
 
 fn test_msgbox(arg1: &str, arg2: &str) {
@@ -180,7 +202,6 @@ fn locate_iat_and_int(import_directory_addr: usize, exe_base_addr: usize, target
     
             // If the module name matches the target module, we return the addresses of both the IAT and the INT
             if module_name_str == target_module {
-                test_msgbox("Found target module: ", module_name_str);
                 let iat_addr = (exe_base_addr  + (*import_descriptor).FirstThunk as usize) as usize;
                 let int_addr = (exe_base_addr  + *(*import_descriptor).u.OriginalFirstThunk_mut() as usize) as usize;
                 return Ok((int_addr, iat_addr));
@@ -193,8 +214,8 @@ fn locate_iat_and_int(import_directory_addr: usize, exe_base_addr: usize, target
     Err(ParseError::ModuleNotFoundError)
 }
 
-// Function to get the address of a specific function in the Import Address Table (IAT) 
-fn get_func_address_in_iat(int_addr: usize, iat_addr: usize, exe_base_addr: usize, target_function: &str) -> Result<usize, ParseError> {
+// Function to get the address of a specific function in the Import Address Table (IAT)
+fn get_func_address_in_iat(int_addr: usize, iat_addr: usize, exe_base_addr: usize, target_function: &str) -> Result<(usize, *mut usize), ParseError> {
     // Cast the addresses as mutable pointers to usize. These pointers are referring to the Import Name Table (INT) and Import Address Table (IAT).
     let mut int_ptr = int_addr as *mut usize;
     let mut iat_ptr = iat_addr as *mut usize;
@@ -216,12 +237,10 @@ fn get_func_address_in_iat(int_addr: usize, iat_addr: usize, exe_base_addr: usiz
                 let func_name_c = CStr::from_ptr((*import_by_name_va).Name.as_ptr());
                 let func_name_str = func_name_c.to_string_lossy();
 
-                test_msgbox("Function imported by name: ", &func_name_str);
-
                 // If the function name matches the target function, return the corresponding address from the IAT. This address is where the application
-                // will jump to when the imported function is called.
+                // will jump to when the imported function is called. Also, return the mutable pointer to the function address in the IAT.
                 if func_name_str == target_function {
-                    return Ok(*iat_ptr);
+                    return Ok((*iat_ptr, iat_ptr));
                 }
             } else {
                 test_msgbox("Function imported by ordinal, skipping.", "");
@@ -237,16 +256,27 @@ fn get_func_address_in_iat(int_addr: usize, iat_addr: usize, exe_base_addr: usiz
     Err(ParseError::FunctionNotFoundError)
 }
 
-// Placeholder function
-fn _perform_hook(_iat: usize) -> usize {
-    // Hook the function and return the address of the original function
-    0 // Placeholder return
+// Overwrite the address being pointed to by target_func_addr_ptr with the address of hook_func_addr
+fn perform_hook(target_func_addr: *mut usize, hook_func_addr: usize) {
+    unsafe {
+        *target_func_addr = hook_func_addr;   
+    }
 }
 
-fn _hook_function(_arg: usize, ) -> usize {
-    // Perform the process on arg
-    // Call the original function
-    0 // Placeholder return
+#[no_mangle]
+pub unsafe extern "system" fn hook_func(hWnd: HWND, lpText: LPCSTR, lpCaption: LPCSTR, uType: UINT) -> winapi::ctypes::c_int {
+    // Convert the LPCSTR to a Rust string
+    let c_str: &CStr = CStr::from_ptr(lpText);
+    let str_slice: &str = c_str.to_str().unwrap();
+    
+    // Your custom message
+    let custom_message = format!("This is what the next message box will say: {}", str_slice);
+    
+    // Display your custom message
+    winapi::um::winuser::MessageBoxA(hWnd, custom_message.as_ptr() as LPCSTR, "Pre-alert".as_ptr() as LPCSTR, uType);
+    
+    // Display the original MessageBox
+    winapi::um::winuser::MessageBoxA(hWnd, lpText, lpCaption, uType)
 }
 
 
